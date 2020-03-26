3583c2692e0af4edf55123c54925b7f37cd9d216
joker <cyp_fly@126.com>
2020-03-26 17:05:28 +0800

    add queue test

--- queue_test.c
+++ queue_test.c
@@ -3,7 +3,23 @@
 
 #include "queue.h"
 
+static void test()
+{
+	Queue Q;
+	unsigned int i;
+	int nums[] = {90, 21, 89, 786, 23, 45, 34, 890, 100, 123, 145, 178, 321, 346, 754, 908, 1024, 2434};
+
+	Q = CreateQueue(0x100);
+	for (i = 0; i < sizeof(nums)/sizeof(int); i++)
+		Enqueue(nums[i], Q);
+	while (!IsEmpty(Q))
+		fprintf(stdout, "%d ", FontAndDequeue(Q));
+	putchar('\n');
+}
+
 int main(void)
 {
+	test();
+
 	return 0;
 }
